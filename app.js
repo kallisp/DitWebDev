22e0a87f7cabd17dd1083bb807459459856b581d
kallisp <kallispyrou@gmail.com>
2021-02-11 14:43:43 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,115 @@
+window.addEventListener('load', (event) => {
+
+    const map = L.map('mapId').setView([37.90, 23.60], 10.5);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const ws = new WebSocket("ws://34.107.39.66:3000");
+
+    const vessels = {};
+    const circles = {};
+    const polylines = {};
+
+    ws.addEventListener('open', (event) => {
+        console.log('Connection established.')
+    });
+
+    ws.addEventListener('close', (event) => {
+        console.log(`Connection closed with message ${event.reason} and code ${event.code}.`);
+    });
+
+    ws.addEventListener('error', (event) => {
+        console.log(`Connection closed with error ${event}.`)
+    });
+
+    ws.addEventListener('message', (event) => {
+        console.log(`Server sent message: ${event.data}`);
+        let msg = [];
+        try {
+            msg = JSON.parse(event.data);
+        }
+        catch (error) {
+            console.error(`An error has occurred: ${error.message}.`);
+        }
+
+        for (let i = 0; i <= msg.length - 1; i++) {
+            const vessel = msg[i];
+
+            const vesselId = vessel.id;
+
+            if (!(vesselId in vessels)) {
+                vessels[vesselId] = [vessel];
+            }
+            else {
+                vessels[vesselId].push(vessel);
+            }
+        }
+
+        for (const vesselId in vessels) {
+            let latlngs = [];
+            let circleLL = [];
+
+            const vesselArray = vessels[vesselId];
+
+            for (let i = 0; i <= vesselArray.length - 1; i++) {
+
+                const vesselInstance = vesselArray[i];
+
+                const lat = vesselInstance.lat;
+                const lon = vesselInstance.lon;
+
+                latlngs.push([lat, lon])
+            }
+
+            const lastVessel = vesselArray[vesselArray.length - 1];
+            circleLL = [lastVessel.lat, lastVessel.lon];
+
+            const popupContent = `<b>Name: </b> ${lastVessel.name}`;
+
+            const popup = L.popup().setContent(popupContent);
+
+            if (vesselId in circles) {
+                const oldCircle = circles[vesselId];
+                oldCircle.setLatLng(circleLL);
+            }
+            else {
+                const newCircle = L.circleMarker(circleLL, {
+                    color: '#E04836',
+                    fillColor: 'white',
+                    fillOpacity: 0.3,
+                    radius: 4,
+                    weight: 1.5
+                }).addTo(map);
+
+                circles[vesselId] = newCircle;
+                newCircle.bindPopup(popup);
+            }
+
+            if (vesselId in polylines) {
+                const oldPolyline = polylines[vesselId];
+                oldPolyline.setLatLngs(latlngs);
+            }
+            else {
+                const polyline = L.polyline(latlngs, {
+                    color: '#E04836',
+                    weight: 1.5
+                }).addTo(map);
+
+                polylines[vesselId] = polyline;
+            }
+        }
+
+        if (msg.length > 0) {
+            const date = new Date(msg[0].timestamp * 1000);
+            const dateString = date.toUTCString();
+
+            const timestampDiv = document.getElementById('timestampId');
+            const timestampElem = document.createElement('p');
+            const timestampText = document.createTextNode(dateString);
+            timestampElem.appendChild(timestampText);
+            timestampDiv.innerHTML = timestampElem.textContent;
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());  // parse application/json
+
+const cors = require('cors');
+app.use(cors());
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('AIS2.sqlite');
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS vessels (timestamp TEXT,
+        mmsi TEXT,
+        imo TEXT,
+        navigational_status TEXT,
+        longitude TEXT,
+        latitude TEXT,
+        heading TEXT,
+        cog TEXT,
+        sog TEXT,
+        ship_name TEXT,
+        call_sign TEXT,
+        ship_type TEXT,
+        draught TEXT,
+        size_bow TEXT,
+        size_stern TEXT,
+        size_port TEXT,
+        size_starboard TEXT,
+        destination TEXT)`);
+
+    const results = [];
+
+    fs.createReadStream('day_test.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            console.log(results);
+            // ,mmsi,imo,navigational_status,longitude,latitude,heading,cog,sog,ship_name,call_sign,ship_type,draught,size_bow,size_stern,size_port,size_starboard,destination
+            // const stmt = db.prepare();
+            results.forEach((result) => {
+                db.run(`INSERT INTO vessels VALUES ($timestamp),($mmsi),($imo),($navigational_status),($longitude),($latitude),($heading),($cog),($sog),($ship_name),($call_sign),($ship_type),($draught),($size_bow),($size_stern),($size_port),($size_starboard),($destination)`, { result });
+            });
+
+            // stmt.finalize();
+        });
+});
+
+// app.use(express.static('public'));
+let currentSec = 0;
+
+const wsPort = 3000;
+app.listen(wsPort, function () {
+    console.log(`Server running at ${wsPort}`);
+
+    function query(ws) {
+        const q = `select *, (hour*3600)+(min*60)+sec AS total_sec 
+                    FROM (select CAST(substr(time(timestamp),1,2) AS INTEGER) AS hour, 
+                    CAST(substr(time(timestamp),4,2) AS INTEGER) AS min,
+                    CAST(substr(time(timestamp),7,2) AS INTEGER) AS sec,
+                    mmsi  AS id, imo, navigational_status, longitude AS lon,latitude AS lat, heading, cog, sog, 
+                    ship_name AS name, call_sign, ship_type, draught, size_bow, size_stern, size_port, size_starboard, destination, strftime('%s',timestamp) AS timestamp
+                    from vessels)                    
+                    where (total_sec>=${currentSec} AND total_sec<(${currentSec}+60))`;
+
+        db.all(q, (err, rows) => {
+            if (err) {
+                // res.json({'message':'failure','code':err});
+                console.error(err);
+                return;
+            }
+            const msg = JSON.stringify(rows);
+            ws.send(msg);
+            currentSec += 60;
+        });
+    };
+
+    // app.get('/', function (req, res, next) {
+    //     console.log('get route', req.testing);
+    //     res.end();
+    // });
+
+    app.ws('/', (ws, req) => {
+        console.log('Ws server connection has opened.');
+        setInterval(() => {
+            query(ws);
+        }, 1000);
+
+        // ws.setInterval(query, 1000);
+        // try {
+        //     const sentMsg = setInterval(query, 1000);
+        //     ws.send(sentMsg);
+        // } catch (err) {
+        //     console.error(err);
+        // }
+
+        // ws.on('message', (message) => {
+        //     const json = JSON.stringify({ message: 'Gotcha' });
+        //     //const input = JSON.parse(msg);
+        //     // ws.send(msg);
+        //     ws.send(json);
+        //     console.log(`Sent: ${json}`);
+        // });
+
+        // The connection was closed
+        ws.on('close', function () {
+            console.log('Ws server connection was closed.');
+        });
+    });
+
+});
+
+
+
+// app.get('/vessels/:destination', async (req, res) => { //οταν κανω ενα get request (req) στο root path θα καλεσει την callback και θα μου επιστρεψει το response (res)
+//     const q = `select * from vessels where destination = '${req.params.destination}'`;
+//     try {
+//         const results = await query(q);
+//         res.send(results);
+//     } catch(err) {
+//         console.error(err);
+//         res.send(err);
+//     }
+// });
+
