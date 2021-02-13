348d8806d87015a033d35b8c07d0cc08214eaeb0
kallisp <kallispyrou@gmail.com>
2021-02-13 19:02:50 +0200

    url http://34.107.39.66:3000

--- server.js
+++ server.js
@@ -13,14 +13,17 @@ app.use(express.static('public'));
 const wsPort = 3000;
 
 function query(ws, currentSec, success) {
-    const q = `select *, (hour*3600)+(min*60)+sec AS total_sec 
-        FROM (select CAST(substr(time(timestamp),1,2) AS INTEGER) AS hour, 
-                    CAST(substr(time(timestamp),4,2) AS INTEGER) AS min,
-                    CAST(substr(time(timestamp),7,2) AS INTEGER) AS sec,
-                    mmsi  AS id, imo, navigational_status, longitude AS lon, latitude AS lat, heading, cog, sog, ship_name AS name, call_sign, 
-                    ship_type, draught, size_bow, size_stern, size_port, size_starboard, destination, strftime('%s',timestamp) AS timestamp
-                    from vessels)                    
-                    where (total_sec>=${currentSec} AND total_sec<(${currentSec}+60))`;
+    const q = `SELECT mmsi AS id, imo, navigational_status, longitude AS lon, latitude AS lat, heading, cog, sog, ship_name AS name, call_sign, ship_type, draught, size_bow, size_stern, size_port, size_starboard, destination, strftime('%s',timestamp) AS timestamp
+                FROM
+                    (SELECT *, (hour*3600)+(min*60)+sec AS total_sec 
+                    FROM
+                        (SELECT CAST(strftime('%H',timestamp) AS INTEGER) AS hour, 
+                                CAST(strftime('%M',timestamp) AS INTEGER) AS min,
+                                CAST(strftime('%S',timestamp) AS INTEGER) AS sec, 
+                                mmsi, imo, navigational_status, longitude, latitude, heading, cog, sog, Ship_name, call_sign, ship_type, draught, size_bow, size_stern, size_port, size_starboard, destination, timestamp
+                        FROM vessels)                    
+                    WHERE (total_sec>=${currentSec} AND total_sec<(${currentSec}+60)))`;
+
 
     db.all(q, (err, rows) => {
         if (err) {
@@ -35,7 +38,7 @@ function query(ws, currentSec, success) {
     });
 };
 
-function getVessels(ws){
+function getVessels(ws) {
     let currentSec = 0;
     setInterval(() => {
         query(ws, currentSec, () => {
@@ -45,20 +48,23 @@ function getVessels(ws){
 };
 
 app.ws('/', (ws, req) => {
-    console.log('Ws server connection has opened.');
+    console.log('Client connection has opened.');
     try {
-       getVessels(ws);
+        getVessels(ws);
     } catch (err) {
         console.error(err);
     };
 
+    ws.on('close', () => {
+        console.log('Client connection was closed.');
+    });
 
-    ws.on('close', function () {
-        console.log('Ws server connection was closed.');
+    ws.on('error', (event) => {
+        console.log(`Client connection closed with error ${event}.`);
     });
 });
 
-app.listen(wsPort, function () {
+app.listen(wsPort, () => {
     console.log(`Server running at port ${wsPort}`);
 });
 
